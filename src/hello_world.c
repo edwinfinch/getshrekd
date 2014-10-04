7d4bca9a58fa8597df56aa899936b6471bcab12a
Edwin Finch <edwinfinch8@gmail.com>
2014-10-04 14:48:14 -0700

    Initial commit

---
+++ src/hello_world.c
@@ -0,0 +1,17 @@
+/*
+Get Shrek'd
+A pebble "virus" that crashes the firmware
+
+(It's a happy face!)
+*/
+#include <pebble.h>
+										/*------------------*/			  /*------------------*/
+                                                Window *w;                     int main()
+                                           {w=window_create();              window_stack_push(
+											       w,1);                         while(	
+											   						//
+												                  //  //
+											         //								//
+												       2==2){                main();} 
+											                app_event_loop();}
+																/////////
